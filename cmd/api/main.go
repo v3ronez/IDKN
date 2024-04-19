4d39ff2c3b4c2867a8ed5409c83d3adec5a600e7
Henrique Veronez <v3ronez.dev@gmail.com>
2024-04-19 12:01:39 -0300

    init app with config struct

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -19,13 +19,17 @@ import (
 const version = "1.0"
 
 type dbConfig struct {
-	host     string
-	dbname   string
-	port     int64
-	user     string
-	password string
-	sslmode  string
+	host         string
+	dbname       string
+	port         int64
+	user         string
+	password     string
+	sslmode      string
+	maxOpenConns int
+	maxIdleConns int
+	maxIndleTime string
 }
+
 type config struct {
 	servPort int
 	envMode  string
@@ -40,9 +44,16 @@ func main() {
 	if err := initEnv(); err != nil {
 		log.Fatalf("fatal error to read env file. error: %s", err)
 	}
+	app := &application{}
+	config := &config{}
+	flag.IntVar(&config.servPort, "port", 8000, "API server port")
+	flag.StringVar(&config.envMode, "env", "dev", "Environment (dev|staging|production)")
+	flag.IntVar(&config.db.maxOpenConns, "db-max-open-conns", 25, "set default value to db max open conns")
+	flag.IntVar(&config.db.maxIdleConns, "db-max-idle-conns", 25, "set default value to db max idle conns")
+	flag.StringVar(&config.db.maxIndleTime, "db-max-idle-time", "15m", "set default value db to idle time conn")
+	flag.Parse()
 
-	app, err := initConfigApp()
-	if err != nil {
+	if err := initConfigApp(app, config); err != nil {
 		panic(err)
 	}
 
@@ -68,35 +79,33 @@ func main() {
 	}
 }
 
-func initConfigApp() (*application, error) {
+func initConfigApp(app *application, cfg *config) error {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 	port, err := strconv.ParseInt(os.Getenv("DB_PORT"), 10, 32)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	dbConfig := dbConfig{
-		user:     os.Getenv("DB_USER"),
-		password: os.Getenv("DB_PASSWORD"),
-		host:     os.Getenv("DB_HOST"),
-		port:     port,
-		dbname:   os.Getenv("DB_DATABASE"),
-		sslmode:  os.Getenv("DB_SSL_MODE"),
-	}
-	app := &application{
-		config: config{
-			db: dbConfig,
-		},
-		logger: logger,
-	}
-	flag.IntVar(&app.config.servPort, "port", 8000, "API server port")
-	flag.StringVar(&app.config.envMode, "env", "dev", "Environment (dev|staging|production)")
-	flag.Parse()
-	return app, nil
+	cfg.db.user = os.Getenv("DB_USER")
+	cfg.db.password = os.Getenv("DB_PASSWORD")
+	cfg.db.host = os.Getenv("DB_HOST")
+	cfg.db.port = port
+	cfg.db.dbname = os.Getenv("DB_DATABASE")
+	cfg.db.sslmode = os.Getenv("DB_SSL_MODE")
+
+	app.config = *cfg
+	app.logger = logger
+	return nil
 }
 
 func initDB(cfg *config) (*sql.DB, error) {
 	dsn := fmt.Sprintf("user=%s password=%s host=%s port=%d dbname=%s sslmode=%s",
-		cfg.db.user, cfg.db.password, cfg.db.host, cfg.db.port, cfg.db.dbname, cfg.db.sslmode)
+		cfg.db.user,
+		cfg.db.password,
+		cfg.db.host,
+		cfg.db.port,
+		cfg.db.dbname,
+		cfg.db.sslmode)
+
 	conn, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, err
@@ -109,7 +118,6 @@ func initDB(cfg *config) (*sql.DB, error) {
 	}
 
 	fmt.Println("db connection success! 🐭")
-	initEnv()
 	return conn, nil
 }
 
