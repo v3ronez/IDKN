110d8a69d2b881f7c4bbf7ef5ee01df0243c1975
Henrique Veronez <v3ronez.dev@gmail.com>
2024-05-06 15:04:17 -0300

    requireactivatedUser middle

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,21 +9,21 @@ import (
 func (app *application) routes() *chi.Mux {
 	routes := chi.NewRouter()
 	routes.Use(app.recoverPanic)
-	// routes.Use(app.rateLimit)
 	routes.Use(app.rateLimitPerClient)
+	routes.Use(app.authenticate)
 	routes.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		app.notFoundResponse(w, r)
 	})
 	routes.MethodNotAllowed(func(w http.ResponseWriter, r *http.Request) {
 		app.methodNotAllowedResponse(w, r)
 	})
-	routes.Get("/v1/healthcheck", app.healthcheckHandler)
-	routes.Get("/v1/movies/{ID}", app.showMovieHandler)
-	routes.Get("/v1/movies", app.listMoviesHandler)
+	routes.Get("/v1/healthcheck", app.requireActivatedUser(app.healthcheckHandler))
+	routes.Get("/v1/movies/{ID}", app.requireActivatedUser(app.showMovieHandler))
+	routes.Get("/v1/movies", app.requireActivatedUser(app.listMoviesHandler))
 	// routes.Put("/v1/movies/{ID}", app.updateMovieHandler)
-	routes.Patch("/v1/movies/{ID}", app.updateMovieHandler)
-	routes.Post("/v1/movies", app.createMovieHandler)
-	routes.Delete("/v1/movies/{ID}", app.deleteMovieHandler)
+	routes.Patch("/v1/movies/{ID}", app.requireActivatedUser(app.updateMovieHandler))
+	routes.Post("/v1/movies", app.requireActivatedUser(app.createMovieHandler))
+	routes.Delete("/v1/movies/{ID}", app.requireActivatedUser(app.deleteMovieHandler))
 
 	//user
 	routes.Post("/v1/users", app.registerUserHandler)
